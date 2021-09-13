ab11f8edb1e017b5b7611698734d874dc29b917e
JaquelineCO20 <jackcrzob20@gmail.com>
2021-09-13 18:27:40 -0500

    tarea de ciclo do while

---
+++ proyectos JAVA/Ejericio 1/Ejercicio1/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int a=1,b=2,c=10,Suma,iterador=0;
+        System.out.println(a);
+        System.out.println(b);
+        do {
+            Suma= a+b;
+            System.out.println(Suma);
+            a=b;
+            b=Suma;
+            iterador++;
+        } while (iterador<c);   
+     }
+}
